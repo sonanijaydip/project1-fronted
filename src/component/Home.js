191744d25f0c58181d1877713dea70535abbc08a
unknown <sonanijaydip>
2024-01-25 09:46:28 +0530

    fronted

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
-
+import axios from 'axios';
 import '../component/css/Home.css'
 import { Link } from 'react-router-dom';
 
@@ -8,10 +8,36 @@ import { Link } from 'react-router-dom';
 
 function Home() {
 
+let [name, setname] = useState('')
+let [email, setemail] = useState('');
+let [password, setpassword] = useState('');
+let [confirmPassword, setconfirmPassword] = useState('');
+
+const btnhandler=() => {
+  axios.post('http://localhost:5000/signup',{
+    name : name,
+    email : email,
+    password: password,
+    confirmPassword: confirmPassword
+  })
+  
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+}
+
   return (
     <div className='main'>
       <Container>
-        <Row className="  vh-100 d-flex justify-content-center align-items-center">
+        <Row className=" vh-100 d-flex justify-content-center align-items-center">
           <Col md={8} lg={6} xs={12}>
             <Card className=" px-4">
               <Card.Body>
@@ -20,18 +46,18 @@ function Home() {
                     Logo
                   </h2> */}
                   <div className="mb-3">
-                    <Form className=''>
+                    <div className=''>
                   <h1 className='text-center'>Register</h1>
                       <Form.Group className="mb-3" controlId="Name">
                         <Form.Label className="text-center">Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Name" />
+                        <Form.Control type="text" placeholder="Enter Name" onChange={(e) => {setname (e.target.value)}} />
                       </Form.Group>
 
                       <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label className="text-center">
                           Email address
                         </Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" onChange={(e) => {setemail (e.target.value)}} />
                       </Form.Group>
 
                       <Form.Group
@@ -39,14 +65,14 @@ function Home() {
                         controlId="formBasicPassword"
                       >
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" onChange={(e) => {setpassword (e.target.value)}} />
                       </Form.Group>
                       <Form.Group
                         className="mb-3"
                         controlId="formBasicPassword"
                       >
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" onChange={(e) => {setconfirmPassword (e.target.value)}} />
                       </Form.Group>
                       <Form.Group
                         className="mb-3"
@@ -54,12 +80,12 @@ function Home() {
                       ></Form.Group>
                       <div className="d-grid">
                         {/* <Button variant="primary" type="submit"> */}
-                          <Link to="/login" className='btn btn-primary'>
+                          <div className='btn btn-primary' onClick={btnhandler()}>
                           Create Account
-                          </Link>
+                          </div>
                         {/* </Button> */}
                       </div>
-                    </Form>
+                    </div>
                     <div className="mt-3">
                       <p className="mb-0  text-center">
                         Already have an account??{' '}
--- src/component/login.js
+++ src/component/login.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../component/css/login.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
 
 function Login() {
+
+let [email, setemail] = useState('');
+let [password, setpassword] = useState('');
+
+const btnhandler=() => {
+  axios.get('http://localhost:5000/login',{
+    email : email,
+    password: password
+  })
+  
+
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+}
+
+
   return (
     <>
   {/* <div className='main'> */}
@@ -14,6 +42,7 @@ function Login() {
             <label>Email address</label>
             <input
               type="email"
+              onChange={(e) =>{ setemail(e.target.value)}}
               className="form-control mt-1"
               placeholder="Enter email"
             />
@@ -22,13 +51,14 @@ function Login() {
             <label>Password</label>
             <input
               type="password"
+              onChange={(e) =>{ setpassword(e.target.value)}}
               className="form-control mt-1"
               placeholder="Enter password"
             />
           </div>
           <div className="d-grid gap-2 mt-3">
   
-              <Link to="/viewsidebar" className='btn btn-primary'>Submit</Link>
+              <Link className='btn btn-primary' onClick={btnhandler()} >Submit</Link>
           </div>
           <p className='mt-3'><a href='#'><Link to="/signup">you don't have any account?</Link></a></p>
           <p className="forgot-password text-right mt-2">
